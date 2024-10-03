d5826591f3ac8e01b0c2421d86b4b70b97779b0b
Arslan <arslanamin2737@gmail.com>
2024-10-03 23:23:58 +0500

    update user api route

--- api/index.js
+++ api/index.js
@@ -3,39 +3,51 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+dotenv.config();
 
-//configure dotenv
-dotenv.config();;
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
+  console.log(process.env.MONGO_URL)
 
-//connect to mongodb
-mongoose.connect(process.env.MONGO_URL).then(() =>{
-    console.log("Connected to MongoDB!");
-}).catch((err) => {
-    console.log(err.message);
-});
+  const __dirname = path.resolve();
 
+const app = express();
 
-//initialize express
-const app = express(); 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.listen(3000, () => {
-    console.log("Server started on port 3000!");
+  console.log('Server is running on port 3000!');
 });
 
-//test api route
-app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter);
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 
-//middleware to handle error
-app.use((error, req, res, next) => {
-    const statusCode = error.statusCode || 500;
-    const message = error.message || "Internal Server Error";
-    return res.status(statusCode).json({
-        status: "error", 
-        statusCode,
-        message: message
-    });
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
